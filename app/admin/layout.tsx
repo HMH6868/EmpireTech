5816c841b9ffe8b10b66283aaa4c4aff78dc0ddd
HUA MINH HOANG <huaminhhoang9999@gmail.com>
2025-11-14 16:55:07 +0700

    FIX : lõi khi build

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,17 @@
 import { AdminSidebar } from '@/components/admin-sidebar';
 import { LanguageProvider } from '@/components/language-provider';
 import type React from 'react';
+import { Suspense } from 'react';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <LanguageProvider>
-      <div className="flex min-h-screen">
-        <AdminSidebar />
-        <main className="flex-1 md:pl-64">{children}</main>
-      </div>
-    </LanguageProvider>
+    <Suspense fallback={null}>
+      <LanguageProvider>
+        <div className="flex min-h-screen">
+          <AdminSidebar />
+          <main className="flex-1 md:pl-64">{children}</main>
+        </div>
+      </LanguageProvider>
+    </Suspense>
   );
 }
